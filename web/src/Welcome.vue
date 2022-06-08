3b05a4370622e46426a04ca27ea65edd0ece5072
wu_hao_ran <2338166429@qq.com>
2022-06-08 12:36:17 +0000

    update web/src/Welcome.vue. 修改首页样式

--- web/src/Welcome.vue
+++ web/src/Welcome.vue
@@ -72,7 +72,6 @@
 <script>
 module.exports = {
 };
-</script>
 <style scoped>
     .container {
             display: block;
@@ -91,20 +90,107 @@ module.exports = {
     .container .box .item {
         margin: 20px;
         border-radius: 5px;
-        text-align: center;
         width: 30%;
-        height: 120;
-        background-color: aqua;
-        line-height: 80px;
+        height: 100px;
+        background-color: rgb(139, 245, 245);
+    }
+    .one{
+        font-size: 36px;
+        display: flex;
+        margin: 10px 20px;
+        justify-content: flex-end;
+    }
+    .one1{
+        font-size: 10px;
+        margin: 0px 0px 0px 20px;
+    }
+    .one2{
+        margin: 0px 0px 0px 20px;
+    }
+    .one3{
+        font-size: 10px;
+        margin: 56px 0px 0px 20px;
+    }
+    .two{
+        width: 100%;
+        display: flex;
+        align-content: center;
+        align-items: center;
+        justify-content: space-around;
+    }
+    .t1{
+        line-height: 40px;
+    }
+    .t2{
+        width: 400px;
+        height: 40px;
+        font-size: 10px;
     }
     .container .box .item2 {
         border-radius: 5px;
         margin: 20px;
         background-color: blanchedalmond;
         height: 120px;
-        width: 40%;
+        width: 65%;
     }
-    .container .box .item2:first-child {
+    .container .box .item3 {
+        border-radius: 5px;
+        margin: 20px;
+        background-color: blanchedalmond;
+        height: 120px;
+        width: 30%;
+    }
+    .three{
+        display: flex;
+        width: 100%;
+        justify-content: space-around;
+        line-height: 30px;
+    }
+    .sh{
+        display: flex;
+        width: 90%;
+        justify-content: space-between;
+        border-bottom: 1px solid slategrey;
+        font-size: 13px;
+    }
+    .three1{
+        display: flex;
+        width: 100%;
+        justify-content: space-evenly;
+        margin: 5px 0px;
+        line-height: 20px;
+    }
+    .sh1{
+        width: 40px;
+        height: 40px;
+        background-color: darkgrey;
+    }
+    .sh2{
+        font-size: 10px;
+    }
+    .sh3{
+        display: flex;
+        width: 160px;
+        height: 20px;
+        justify-content: space-between;
+        font-size: 10px;
+    }
+    .image1{
+        width: 40px;
+        height: 40px;
+    }
+    .image2{
+        margin: 10px 0px 0px 10px;
+        width: 95%;
+        height: 60%;
+    }
+    .image3{
+        margin: 10px 0px 0px 10px;
+        width: 90%;
+        height: 60%;
+    }
+
+    /* .container .box .item2:first-child {
         width: 55%;
-    }
+    } */
 </style>
